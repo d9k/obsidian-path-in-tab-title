8d9d8ef894ce848c7b1ce69b9242b38200736140
d9k <d9k@ya.ru>
2026-04-22 18:28:32 +0500

    Bump version to 0.0.2 (`npm run version`)

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,7 @@
 import { readFileSync, writeFileSync } from "fs";
 
 const targetVersion = process.env.npm_package_version;
+console.log('targetVersion:', targetVersion);
 
 // read minAppVersion from manifest.json and bump version to target version
 const manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
